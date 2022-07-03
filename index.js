9c001d802daf7278c16614fcfb88f137cede6691
Roberto <mario24roberto@gmail.com>
2022-07-03 09:21:22 -0600

    Habilitando pug

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ import usuariosRoutes from './routes/UsuarioRoutes.js';
 // Crear la App
 
 const app = express();
-
+app.set('view engine', 'pug');
+app.set('views', './views');
 // Definir Puerto
 app.use('/', usuariosRoutes);
 const port = 3000;
+
+// Habilitar pug
+
 
 app.listen(port,()=>{
     console.log(`El servidor esta funcionando en el puerto ${port}`)

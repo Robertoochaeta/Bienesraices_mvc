9d2a9ece495bebc1317abf31c5cfc369d2db033f
Roberto <mario24roberto@gmail.com>
2022-07-04 14:17:33 -0600

    Agregando un segundo controlador y creando las vistas

---
+++ controllers/usuarioController.js
@@ -0,0 +1,13 @@
+const formularioLogin = (res,req)=>{
+    res.render('auth/login',{
+       
+    })
+}
+const formularioRegustri = (res,req)=>{
+    res.render('auth/registro',{
+        
+    })
+}
+export {
+    formularioLogin,
+}
